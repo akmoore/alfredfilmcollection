0983838ede08133bdb298e34267b993d09b8645d
Alfred Moore <ak_moore@live.com>
2023-01-07 00:49:42 -0600

    Added icons.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,11 +2,22 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    
+    <link rel="shortcut icon" href="alfredfilms_x180.png">
+    <link rel="apple-touch-icon" href="alfredfilms_x180.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="alfredfilms_x180.png">
+    <link rel="icon" sizes="192x192" href="alfredfilms_x192.png"/>
+    <link rel="icon" sizes="512x512" href="alfredfilms_x512.png"/>
+    <link rel="icon" type="image/png" sizes="64x64" href="alfredfilms_x64.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="alfredfilms_x32.png"/>
+    <link rel="icon" type="image/png" sizes="16x16" href="alfredfilms_x16.png"/>
+
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/font-awesome@4.x/css/font-awesome.min.css" rel="stylesheet"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <link href="https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css" rel="stylesheet">
-    <!-- <link href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" rel="stylesheet"> -->
     @routes
     @vite('resources/js/app.js')
     <title>Ken Movies</title>
